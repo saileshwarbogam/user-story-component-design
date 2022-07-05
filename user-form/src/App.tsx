2a67562e8a29ad5a38618b1e87974001eab3fcb3
saileshwar.bogam <saileshwar.bogam@gmail.com>
2022-07-05 12:54:18 +0530

    user-form

---
+++ user-form/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { UserForm } from './components/UserTextField/UserForm';
+
+function App() {
+  return (
+    <div>
+      <UserForm/>
+    </div>
+  );
+}
+
+export default App;
